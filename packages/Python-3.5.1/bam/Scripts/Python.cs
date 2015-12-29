22a81031b148045120f996316f64f32815586489
Mark Final <markfinal@hotmail.com>
2015-12-29 09:25:49 +0000

    [packages,Python,3.5.1] Adding module for the Python interpreter (Windows only so far)

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -1,5 +1,32 @@
 using Bam.Core;
 namespace Python
 {
-    // write modules here ...
+    sealed class PythonInterpreter :
+        C.ConsoleApplication
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.Macros["OutputName"] = Bam.Core.TokenizedString.CreateVerbatim("python");
+
+            var source = this.CreateCSourceContainer("$(packagedir)/Programs/python.c");
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    compiler.PreprocessorDefines.Add("Py_BUILD_CORE");
+                    compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/Include"));
+                    if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/PC"));
+                    }
+                });
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+            {
+                this.CompilePubliclyAndLinkAgainst<WindowsSDK.WindowsSDK>(source);
+            }
+        }
+    }
 }
