3578160d88bfd39f232a3bd3e9b215f7fcaebf48
Mark Final <markfinal@hotmail.com>
2015-12-29 10:05:11 +0000

    [packages,Python,3.5.1] Add the parser source for the Python dynamic library

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -27,6 +27,41 @@ namespace Python
             {
                 this.CompilePubliclyAndLinkAgainst<WindowsSDK.WindowsSDK>(source);
             }
+
+            this.LinkAgainst<PythonLibrary>();
+        }
+    }
+
+    sealed class PythonLibrary :
+        C.DynamicLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.Macros["OutputName"] = Bam.Core.TokenizedString.CreateVerbatim("python");
+            this.Macros["MajorVersion"] = Bam.Core.TokenizedString.CreateVerbatim("3");
+            this.Macros["MinorVersion"] = Bam.Core.TokenizedString.CreateVerbatim("5");
+            this.Macros["PatchVersion"] = Bam.Core.TokenizedString.CreateVerbatim("1");
+
+            var parserSource = this.CreateCSourceContainer("$(packagedir)/Parser/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*pgen).*)$"));
+            parserSource.PrivatePatch(settings =>
+            {
+                var compiler = settings as C.ICommonCompilerSettings;
+                compiler.PreprocessorDefines.Add("Py_BUILD_CORE");
+                compiler.PreprocessorDefines.Add("Py_ENABLE_SHARED");
+                compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/Include"));
+                if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+                {
+                    compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/PC"));
+                }
+            });
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+            {
+                this.CompilePubliclyAndLinkAgainst<WindowsSDK.WindowsSDK>(parserSource);
+            }
         }
     }
 }
