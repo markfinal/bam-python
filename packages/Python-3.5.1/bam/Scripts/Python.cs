5056d22ff90a790d6fe2ba3cd758528533c4584e
Mark Final <markfinal@hotmail.com>
2015-12-31 13:23:53 +0000

    [packages,Python,3.5.1] Attempted at distributing the Python lib... wrong name though

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -533,6 +533,8 @@ namespace Python
 
             var app = this.Include<PythonInterpreter>(C.ConsoleApplication.Key, EPublishingType.ConsoleApplication);
             this.Include<PythonLibrary>(C.DynamicLibrary.Key, ".", app);
+
+            this.IncludeDirectory(this.CreateTokenizedString("$(packagedir)/Lib"), "Lib/python3.5", app);
         }
     }
 }
