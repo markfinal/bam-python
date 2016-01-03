3894e3913efc878912b785f7bd335c76302d2a22
Mark Final <markfinal@hotmail.com>
2016-01-03 21:55:09 +0000

    [packages,Python,3.5.1] Initialize builtin modules on Posix platforms

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -204,6 +204,79 @@ namespace Python
             }
         }
 
+        private void
+        insertBuiltinModules(
+            ref string configText)
+        {
+            var declarations = new System.Text.StringBuilder();
+            var inittab = new System.Text.StringBuilder();
+
+            declarations.AppendLine("extern PyObject* PyInit_posix(void);");
+            inittab.AppendLine("\t{\"posix\", PyInit_posix},");
+
+            declarations.AppendLine("extern PyObject* PyInit_errno(void);");
+            inittab.AppendLine("\t{\"errno\", PyInit_errno},");
+
+            declarations.AppendLine("extern PyObject* PyInit_pwd(void);");
+            inittab.AppendLine("\t{\"pwd\", PyInit_pwd},");
+
+            declarations.AppendLine("extern PyObject* PyInit__sre(void);");
+            inittab.AppendLine("\t{\"_sre\", PyInit__sre},");
+
+            declarations.AppendLine("extern PyObject* PyInit__codecs(void);");
+            inittab.AppendLine("\t{\"_codecs\", PyInit__codecs},");
+
+            declarations.AppendLine("extern PyObject* PyInit__weakref(void);");
+            inittab.AppendLine("\t{\"_weakref\", PyInit__weakref},");
+
+            declarations.AppendLine("extern PyObject* PyInit__functools(void);");
+            inittab.AppendLine("\t{\"_functools\", PyInit__functools},");
+
+            declarations.AppendLine("extern PyObject* PyInit__operator(void);");
+            inittab.AppendLine("\t{\"_operator\", PyInit__operator},");
+
+            declarations.AppendLine("extern PyObject* PyInit__collections(void);");
+            inittab.AppendLine("\t{\"_collections\", PyInit__collections},");
+
+            declarations.AppendLine("extern PyObject* PyInit_itertools(void);");
+            inittab.AppendLine("\t{\"itertools\", PyInit_itertools},");
+
+            declarations.AppendLine("extern PyObject* PyInit_atexit(void);");
+            inittab.AppendLine("\t{\"atexit\", PyInit_atexit},");
+
+            declarations.AppendLine("extern PyObject* PyInit__stat(void);");
+            inittab.AppendLine("\t{\"_stat\", PyInit__stat},");
+
+            declarations.AppendLine("extern PyObject* PyInit_time(void);");
+            inittab.AppendLine("\t{\"time\", PyInit_time},");
+
+            declarations.AppendLine("extern PyObject* PyInit__locale(void);");
+            inittab.AppendLine("\t{\"_locale\", PyInit__locale},");
+
+            declarations.AppendLine("extern PyObject* PyInit__io(void);");
+            inittab.AppendLine("\t{\"_io\", PyInit__io},");
+
+            declarations.AppendLine("extern PyObject* PyInit_zipimport(void);");
+            inittab.AppendLine("\t{\"zipimport\", PyInit_zipimport},");
+
+            declarations.AppendLine("extern PyObject* PyInit_faulthandler(void);");
+            inittab.AppendLine("\t{\"defaulthandler\", PyInit_faulthandler},");
+
+            declarations.AppendLine("extern PyObject* PyInit__tracemalloc(void);");
+            inittab.AppendLine("\t{\"_tracemalloc\", PyInit__tracemalloc},");
+
+            declarations.AppendLine("extern PyObject* PyInit__symtable(void);");
+            inittab.AppendLine("\t{\"_symtable\", PyInit__symtable},");
+
+            declarations.AppendLine("extern PyObject* PyInit__signal(void);");
+            inittab.AppendLine("\t{\"_signal\", PyInit__signal},");
+
+            configText = configText.Replace("/* -- ADDMODULE MARKER 1 -- */",
+                declarations.ToString());
+            configText = configText.Replace("/* -- ADDMODULE MARKER 2 -- */",
+                inittab.ToString());
+        }
+
         protected override void
         ExecuteInternal(
             ExecutionContext context)
@@ -216,6 +289,7 @@ namespace Python
             }
             var stubPath = this.CreateTokenizedString("$(packagedir)/Modules/config.c.in").Parse();
             var stubText = System.IO.File.ReadAllText(stubPath);
+            insertBuiltinModules(ref stubText);
             using (System.IO.TextWriter writeFile = new System.IO.StreamWriter(destPath))
             {
                 writeFile.Write(stubText);
@@ -397,6 +471,7 @@ namespace Python
             moduleSource.AddFiles("$(packagedir)/Modules/_heapqmodule.c");
             moduleSource.AddFiles("$(packagedir)/Modules/_io/*.c");
             moduleSource.AddFiles("$(packagedir)/Modules/_json.c");
+            moduleSource.AddFiles("$(packagedir)/Modules/_localemodule.c");
             moduleSource.AddFiles("$(packagedir)/Modules/_lsprof.c");
             moduleSource.AddFiles("$(packagedir)/Modules/_math.c");
             moduleSource.AddFiles("$(packagedir)/Modules/_opcode.c");
@@ -411,13 +486,13 @@ namespace Python
             moduleSource.AddFiles("$(packagedir)/Modules/_weakref.c");
             if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
             {
-                moduleSource.AddFiles("$(packagedir)/Modules/_localemodule.c");
                 moduleSource.AddFiles("$(packagedir)/Modules/_winapi.c");
                 moduleSource.AddFiles("$(packagedir)/PC/msvcrtmodule.c");
             }
             else
             {
                 moduleSource.AddFiles("$(packagedir)/Modules/getpath.c");
+                moduleSource.AddFiles("$(packagedir)/Modules/pwdmodule.c");
 
                 var configSource = Bam.Core.Graph.Instance.FindReferencedModule<ConfigSource>();
                 moduleSource.AddFile(configSource);
