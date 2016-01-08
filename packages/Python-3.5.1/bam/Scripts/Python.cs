b7396983946dd1b4784b8782ff63be88c844676a
Mark Final <markfinal@hotmail.com>
2016-01-08 16:49:13 +0000

    [packages,Python,3.5.1] Made quoted strings consistent on the processor definitions

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -496,8 +496,8 @@ namespace Python
                     item.PrivatePatch(settings =>
                         {
                             var compiler = settings as C.ICommonCompilerSettings;
-                            compiler.PreprocessorDefines.Add("VERSION", "\\\"3.5\\\"");
-                            compiler.PreprocessorDefines.Add("PYTHONPATH", "\\\".\\\"");
+                            compiler.PreprocessorDefines.Add("VERSION", "\"3.5\"");
+                            compiler.PreprocessorDefines.Add("PYTHONPATH", "\".\"");
                         }));
             }
 
