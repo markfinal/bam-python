14b0116b6255bb1d5627aeddfece540ea2db790a
Mark Final <markfinal@hotmail.com>
2016-01-12 14:38:40 +0000

    [packages,Python,3.5.1] Python interpreter shell does not need Py_BUILD_CORE

--- packages/Python-3.5.1/bam/Scripts/Python.cs
+++ packages/Python-3.5.1/bam/Scripts/Python.cs
@@ -14,11 +14,6 @@ namespace Python
             this.Macros["OutputName"] = Bam.Core.TokenizedString.CreateVerbatim("python");
 
             var source = this.CreateCSourceContainer("$(packagedir)/Programs/python.c");
-            source.PrivatePatch(settings =>
-                {
-                    var compiler = settings as C.ICommonCompilerSettings;
-                    compiler.PreprocessorDefines.Add("Py_BUILD_CORE");
-                });
             if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
             {
                 this.CompilePubliclyAndLinkAgainst<WindowsSDK.WindowsSDK>(source);
