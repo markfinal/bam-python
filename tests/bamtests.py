b0f0ef6bfd4a55105e66383ac4e976c9097c40b3
Mark Final <markfinal@hotmail.com>
2016-01-12 17:28:31 +0000

    [tests] Adding automated test configuration file

---
+++ tests/bamtests.py
@@ -0,0 +1,8 @@
+from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+
+def configure_repository():
+    configs = {}
+    configs["InterpreterTest1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64]},
+                                            linux={"Native":[gcc64],"MakeFile":[gcc64]},
+                                            osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
+    return configs
