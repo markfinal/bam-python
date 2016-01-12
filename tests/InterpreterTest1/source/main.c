726a14237573d552f10779ba7e96da8a1caea6f6
Mark Final <markfinal@hotmail.com>
2016-01-12 10:32:39 +0000

    [tests,InterpreterTest1] Using Python 3 syntax, not Python 2

--- tests/InterpreterTest1/source/main.c
+++ tests/InterpreterTest1/source/main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     Py_SetProgramName(argv[0]);
     Py_Initialize();
     PyRun_SimpleString("from time import time,ctime\n"
-                       "print 'Today is',ctime(time())\n");
+                       "print('Today is',ctime(time()))\n");
     Py_Finalize();
     return 0;
 }
