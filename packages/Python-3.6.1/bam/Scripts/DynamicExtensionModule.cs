76d75aac797fe1130060e99288276af943141909
Mark Final <markfinal@hotmail.com>
2018-04-20 06:50:20 +0100

    [packages,Python,3.6.1] Fixes #2. Dynamic C extension modules now use the version number from the Python library

--- packages/Python-3.6.1/bam/Scripts/DynamicExtensionModule.cs
+++ packages/Python-3.6.1/bam/Scripts/DynamicExtensionModule.cs
@@ -138,6 +138,10 @@ namespace Python
                 this.Macros["pluginext"] = Bam.Core.TokenizedString.CreateVerbatim(".so");
             }
 
+            this.Macros["MajorVersion"] = Bam.Core.TokenizedString.CreateVerbatim(Version.Major);
+            this.Macros["MinorVersion"] = Bam.Core.TokenizedString.CreateVerbatim(Version.Minor);
+            this.Macros["PatchVersion"] = Bam.Core.TokenizedString.CreateVerbatim(Version.Patch);
+
             this.moduleSourceModules = this.CreateCSourceContainer();
             foreach (var basename in this.SourceFiles)
             {
