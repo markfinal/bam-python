510e1ea13e2b65afe45220aae8e78269e43dd8de
Mark Final <markfinal@hotmail.com>
2018-08-12 21:48:56 +0100

    Predicating out unused policies

--- packages/Python-3.6.1/bam/Scripts/pydoc/Interfaces/IPyDocGenerationPolicy.cs
+++ packages/Python-3.6.1/bam/Scripts/pydoc/Interfaces/IPyDocGenerationPolicy.cs
@@ -27,6 +27,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+#if BAM_V2
+#else
 namespace Python
 {
     public interface IPyDocGenerationPolicy
@@ -41,3 +43,4 @@ namespace Python
             string moduleToDocument);
     }
 }
+#endif
