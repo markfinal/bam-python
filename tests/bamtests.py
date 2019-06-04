0de6f8069c4adab050cdaa29006fa1def7643fe9
Mark Final <markfinal@hotmail.com>
2019-06-04 20:31:21 +0100

    [tests] Disabled ExtensionTest2, enabled InterpreterTest1

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -5,12 +5,12 @@ def configure_repository():
     #configs["ExtensionModuleTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
     #                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
     #                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["ExtensionModuleTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                                linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                                osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    #configs["InterpreterTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-    #                                        linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-    #                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    #configs["ExtensionModuleTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+    #                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+    #                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["InterpreterTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     #configs["InterpreterTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
     #                                        linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
     #                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
